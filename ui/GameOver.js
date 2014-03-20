203e7dfd7de61b18d0d0e63e01cb5a0836846f76
FabianoPS <fabianosoriani@gmail.com>
2014-03-20 13:57:34 -0700

    game restart, copy changes

--- ui/GameOver.js
+++ ui/GameOver.js
@@ -1,16 +1,18 @@
 (function() {
   ui.GameOver = {
     show: function() {
+      alert("You lost! \nGame is restarting")
+      window.location.href = window.location.href
       //var bg = new Phaser.Rectangle(0, 0, SCREEN_W, SCREEN_H);
-      var text = "Game Over";
-      var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+      // var text = "Game Over";
+      // var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
 
-      var center = {
-        x: SCREEN_W/2, 
-        y: WORLD_H - SCREEN_H / 2 - 100
-      };
-      var t = game.add.text(center.x, center.y, text, style);
-      t.anchor.setTo(0.5,0.5);
+      // var center = {
+      //   x: SCREEN_W/2, 
+      //   y: WORLD_H - SCREEN_H / 2 - 100
+      // };
+      // var t = game.add.text(center.x, center.y, text, style);
+      // t.anchor.setTo(0.5,0.5);
     }
   };
 }(ui));
